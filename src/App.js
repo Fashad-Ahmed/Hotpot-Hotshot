e38de861ad5104d02781b8cfa69b6cd6f01bbad6
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2023-09-23 13:56:32 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RoutesProvider from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./fonts/fonts.css";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
